a2dc3b4e5655e71a793ad71b197632bb5163b3bd
unknown <rafaelchavezvictormanuel@gmail.com>
2025-06-11 18:55:28 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3307156,
+    "boundingBox": {
+        "lx": 265142.905,
+        "ly": 2178158.402,
+        "lz": 1874.25,
+        "ux": 265205.279,
+        "uy": 2178220.7759999998,
+        "uz": 1936.6239999999525
+    },
+    "tightBoundingBox": {
+        "lx": 265142.905,
+        "ly": 2178158.402,
+        "lz": 1874.25,
+        "ux": 265205.279,
+        "uy": 2178220.527,
+        "uz": 1898.535
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.540174663066864,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
